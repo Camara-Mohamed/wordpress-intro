afde33a8aa5b23051ad1b6f11bd5db19d64eca4a
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-02-07 12:27:42 +0100

    Initial commit

---
+++ wp-content/themes/dw/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
+</head>
+<body>
+    <h1><?= get_bloginfo('name') ?></h1>
+    <p><?= get_bloginfo('description') ?></p>
+</body>
+</html>
