5b956656e9f1c64a2e687b812b6193198023fd3a
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-02-14 11:55:31 +0100

    Added thumbnail custom

--- wp-content/themes/dw/single-recipe.php
+++ wp-content/themes/dw/single-recipe.php
@@ -1,7 +1,22 @@
 <?php get_header(); ?>
 
 <style>
+.recipe {
+    display: none;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+}
 
+.recipe_ingredients {
+    width: 320px;
+    padding: 20px;
+    background: #0a4b78;
+    display: flex;
+    padding-top: 100%;
+}
+.recipe_img {
+    display: block;
+}
 </style>
 
 <?php
@@ -10,10 +25,18 @@
 if (have_posts()): while (have_posts()): the_post(); ?>
 
     <h2><?= get_the_title(); ?></h2>
+
+    <p><?= get_the_excerpt(); ?></p>
+
     <div class="recipe">
         <aside class="recipe_ingredients">
-            <h3>Ingrédients</h3>
-            <p>À compléter</p>
+            <div>
+                <h3>Ingrédients</h3>
+                <p>À compléter</p>
+            </div>
+            <figure class="recipe_figure">
+                <?= get_post_thumbnail_id(size: 'medium', attr: ['recipe_img']); ?>
+            </figure>
         </aside>
         <section class="recipe_steps">
             <h3>Étapes</h3>
